1f38d835aed8280419cb6daf115b3957163093a0
gitmirgut <gitmirgut@users.noreply.github.com>
2017-03-13 22:08:50 +0100

    build file structure

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='bb_stitcher',
+    version='0.0.0.dev1',
+    description='Stitch images from different cam positions,'
+                'with an affine transformation',
+    long_description='',
+    url='https://github.com/gitmirgut/bb_stitcher',
+    author='gitmirgut',
+    author_email="gitmirgut@users.noreply.github.com",
+    packages=['bb_stitcher'],
+    license='GNU GPLv3',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.5'
+    ]
+)
