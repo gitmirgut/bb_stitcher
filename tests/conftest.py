d5982d85a47fbaa8a66a215ba611f90426f1dd82
gitmirgut <gitmirgut@users.noreply.github.com>
2017-03-18 13:56:48 +0100

    better visual feedback of rectification

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,14 +17,16 @@ def fname(path):
 
 @pytest.fixture
 def left_img():
-    relativ_path = 'data/Cam_0_2016-09-01T12:56:50.801920Z.jpg'
+    img_path = 'data/Cam_0_2016-09-01T12:56:50.801920Z.jpg'
+    img_with_detections = 'data/Cam_0_2016-09-01T12:56:50.801920Z_detections.jpg'
     detections_path = 'data/Cam_0_2016-09-01T12:56:50.801920Z_detections.npy'
     d = dict()
-    d['path'] = get_test_fname(relativ_path)
+    d['path'] = get_test_fname(img_path)
     d['name'] = os.path.basename(os.path.splitext(d['path'])[0])
     d['img'] = cv2.imread(d['path'], -1)
     d['height'], d['width'] = d['img'].shape[:2]
     d['detections'] = np.load(get_test_fname(detections_path))
+    d['img_w_detections'] = cv2.imread(get_test_fname(img_with_detections), -1)
     return d
 
 @pytest.fixture
--- tests/test_preperation.py
+++ tests/test_preperation.py
@@ -4,31 +4,33 @@ import cv2
 import os.path
 import numpy as np
 
-def draw_circle(img, position, radius=32, line_width=6, color=(0, 0, 255)):
-    """Draw circle around position."""
-    cv2.circle(img, tuple(position), radius, color, line_width)
+def draw_makers(img, pts, color=(0, 0, 255), marker_types=cv2.MARKER_CROSS):
+    img_m = np.copy(img)
+    if len(img_m.shape) == 2:
+        img_m = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    pts = pts.astype(int)
+    for pt in pts:
+        cv2.drawMarker(img_m, tuple(pt), color, markerType=marker_types,
+                       markerSize=40, thickness=5)
+    return img_m
 
-def bname(path, discription, ext):
-    basename_no_ext = os.path.basename(os.path.splitext(path)[0])
-    return ''.join([basename_no_ext, '_', discription,'.', ext])
-
-def fname(path):
-    return os.path.basename(os.path.splitext(path)[0])
 
 def test_Rectificator(left_img, config, outdir):
     rectificator = prep.Rectificator(config)
     corrected_image = rectificator.rectify_image(left_img['img'])
     assert corrected_image.shape == left_img['img'].shape
+
+    # for visual see /out
     name_img_rect = ''.join([left_img['name'], '_rectified.jpg'])
     out = os.path.join(outdir, name_img_rect)
     cv2.imwrite(out, corrected_image)
 
     corrected_detections = rectificator.rectify_points(left_img['detections'], left_img['height'], left_img['width'])
     assert len(corrected_detections) == len(left_img['detections'])
-    for pos in corrected_detections:
-        pos = pos.astype(np.int32)
-        draw_circle(corrected_image, pos)
-    name_img_rect_detec = ''.join([left_img['name'], '_rectified_detections.jpg'])
-    out = os.path.join(outdir, name_img_rect_detec)
-    cv2.imwrite(out, corrected_image)
 
+    # for visual see /out
+    corrected_image_w_detections = rectificator.rectify_image(left_img['img_w_detections'])
+    corrected_image_w_detections = draw_makers(corrected_image_w_detections, corrected_detections)
+    name_img_rect_detec = ''.join([left_img['name'], '_detections_rectified.jpg'])
+    out = os.path.join(outdir, name_img_rect_detec)
+    cv2.imwrite(out, corrected_image_w_detections)
