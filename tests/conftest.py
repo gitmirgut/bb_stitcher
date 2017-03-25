73df66ce32e86d3453676f0bd399c95313ae62e8
gitmirgut <gitmirgut@users.noreply.github.com>
2017-03-25 22:17:15 +0100

    add right image data

--- tests/conftest.py
+++ tests/conftest.py
@@ -37,6 +37,22 @@ def left_img():
 
 
 @pytest.fixture
+def right_img():
+    img_path = 'data/Cam_1_2016-09-01T12:56:50.801926Z.jpg'
+    img_with_detections = 'data/Cam_1_2016-09-01T12:56:50.801926Z_detections.jpg'
+    detections_path = 'data/Cam_1_2016-09-01T12:56:50.801926Z_detections.npy'
+    d = dict()
+    d['path'] = get_test_fname(img_path)
+    d['name'] = os.path.basename(os.path.splitext(d['path'])[0])
+    d['img'] = cv2.imread(d['path'], -1)
+    d['height'], d['width'] = d['img'].shape[:2]
+    d['size'] = d['width'], d['height']
+    d['detections'] = np.load(get_test_fname(detections_path))
+    d['img_w_detections'] = cv2.imread(get_test_fname(img_with_detections), -1)
+    return d
+
+
+@pytest.fixture
 def config():
     default_config = configparser.ConfigParser()
     default_config.read(core.get_default_config())
