4861ed7d588086d6342112c64506c84f39b72fcc
gitmirgut <gitmirgut@users.noreply.github.com>
2017-03-25 18:58:46 +0100

    add tests for calc mask

---
+++ tests/test_stitcher.py
@@ -0,0 +1,31 @@
+import numpy as np
+import numpy.testing as npt
+
+import bb_stitcher.stitcher as stitcher
+
+
+def test_calc_feature_mask():
+    target_mask_left = np.array(
+        [[0, 0, 0],
+         [0, 0, 0],
+         [0, 0, 0],
+         [0, 255, 255],
+         [0, 255, 255],
+         [0, 255, 255],
+         [0, 255, 255],
+         [0, 0, 0],
+         ], np.uint8)
+    target_mask_right = np.array(
+        [[0, 0, 0],
+         [0, 0, 0],
+         [0, 0, 0],
+         [255, 255, 0],
+         [255, 255, 0],
+         [255, 255, 0],
+         [255, 255, 0],
+         [0, 0, 0],
+         ], np.uint8)
+    mask_left, mask_right = stitcher.FeatureBasedStitcher._calc_feature_mask(
+        (3, 8), (3, 8), 2, 3, 1)
+    npt.assert_equal(mask_left, target_mask_left)
+    npt.assert_equal(mask_right, target_mask_right)
