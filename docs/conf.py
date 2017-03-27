fd2356d2a3d84ae4dacc5a8c9b72256c6e484659
gitmirgut <gitmirgut@users.noreply.github.com>
2017-03-27 12:42:21 +0200

    add linkcode_resolve to get links to github

--- docs/conf.py
+++ docs/conf.py
@@ -44,6 +44,7 @@ sys.path.insert(0, os.path.abspath('../'))
 extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
+    'sphinx.ext.linkcode',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
@@ -177,3 +178,54 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
+
+
+def linkcode_resolve(domain, info):
+    """
+    Determine the URL corresponding to Python object
+    """
+    import inspect
+    from os.path import relpath, dirname
+    import bb_stitcher
+    if domain != 'py':
+        return None
+
+    modname = info['module']
+    fullname = info['fullname']
+
+    submod = sys.modules.get(modname)
+    if submod is None:
+        return None
+
+    obj = submod
+    for part in fullname.split('.'):
+        try:
+            obj = getattr(obj, part)
+        except:
+            return None
+
+    try:
+        fn = inspect.getsourcefile(obj)
+    except:
+        fn = None
+    if not fn:
+        return None
+
+    try:
+        source, lineno = inspect.findsource(obj)
+    except:
+        lineno = None
+
+    if lineno:
+        linespec = "#L%d" % (lineno + 1)
+    else:
+        linespec = ""
+
+    fn = relpath(fn, start=dirname(bb_stitcher.__file__))
+
+    if 'dev' in bb_stitcher.__version__:
+        return ("http://github.com/gitmirgut/bb_stitcher/blob/"
+                "master/bb_stitcher/%s%s" % (fn, linespec))
+    else:
+        return ("http://github.com/gitmirgut/bb_stitcher/blob/"
+"v%s/bb_stitcher/%s%s" % (bb_stitcher.__version__, fn, linespec))
