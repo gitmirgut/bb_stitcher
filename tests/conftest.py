70af6db7568bf6c70ac0d77bd13c7c34eb1a37f1
gitmirgut <gitmirgut@users.noreply.github.com>
2017-03-29 13:24:45 +0200

    add incremental marker.

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,21 @@ import pytest
 
 import bb_stitcher.core as core
 
+
+def pytest_runtest_makereport(item, call):
+    if "incremental" in item.keywords:
+        if call.excinfo is not None:
+            parent = item.parent
+            parent._previousfailed = item
+
+
+def pytest_runtest_setup(item):
+    if "incremental" in item.keywords:
+        previousfailed = getattr(item.parent, "_previousfailed", None)
+        if previousfailed is not None:
+            pytest.xfail("previous test failed (%s)" %previousfailed.name)
+
+
 test_dir = os.path.dirname(__file__)
 
 
