fb75d4b4ea522a1271d3a042b9688ae1e12c4315
gitmirgut <gitmirgut@users.noreply.github.com>
2017-04-14 21:53:22 +0200

    improve readability

--- tests/test_stitcher.py
+++ tests/test_stitcher.py
@@ -12,8 +12,7 @@ import bb_stitcher.visualisation as vis
 
 @pytest.fixture
 def outdir(main_outdir):
-    # out_path = os.path.join(main_outdir, str(__name__))
-    out_path = os.path.join(main_outdir, 'test_stitcher2')
+    out_path = os.path.join(main_outdir, str(__name__))
     if not os.path.exists(out_path):
         os.makedirs(out_path)
     return out_path
@@ -88,25 +87,32 @@ def test_map_points_angles(left_img, right_img, outdir, config, monkeypatch):
 
     rt_stitcher = stitcher.RectangleStitcher(config)
     rt_stitcher.estimate_transform(left_img['img'], right_img['img'], 90, -90)
+
     assert rt_stitcher.homo_left is not None
     assert rt_stitcher.homo_right is not None
     assert rt_stitcher.pano_size is not None
+
     pano = rt_stitcher.compose_panorama(
         left_img['img_w_detections'], right_img['img_w_detections'])
+
     detections_left_mapped, yaw_angles_left_mapped = rt_stitcher.map_left_points_angles(
         left_img['detections'], left_img['yaw_angles'])
     detections_right_mapped, yaw_angles_right_mapped = rt_stitcher.map_right_points_angles(
         right_img['detections'], right_img['yaw_angles'])
+
     vis.draw_complex_marks(pano, detections_left_mapped, yaw_angles_left_mapped)
     vis.draw_complex_marks(pano, detections_right_mapped, yaw_angles_right_mapped)
+
     out = os.path.join(outdir, 'panorama_rt_w_detections_angles.jpg')
     cv2.imwrite(out, pano)
 
 
 @pytest.mark.slow
 def test_compose_panorama(fb_stitcher, left_img, right_img, outdir):
+
     fb_stitcher.estimate_transform(left_img['img'], right_img['img'], 90, -90)
     pano = fb_stitcher.compose_panorama(left_img['img'], right_img['img'])
+
     out = os.path.join(outdir, 'panorama.jpg')
     cv2.imwrite(out, pano)
 
@@ -135,6 +141,7 @@ def test_calc_feature_mask():
          [0, 255, 255],
          [0, 0, 0],
          ], np.uint8)
+
     target_mask_right = np.array(
         [[0, 0, 0],
          [0, 0, 0],
@@ -145,8 +152,10 @@ def test_calc_feature_mask():
          [255, 255, 0],
          [0, 0, 0],
          ], np.uint8)
+
     mask_left, mask_right = stitcher.FeatureBasedStitcher._calc_feature_mask(
         (3, 8), (3, 8), 2, 3, 1)
+
     npt.assert_equal(mask_left, target_mask_left)
     npt.assert_equal(mask_right, target_mask_right)
 
@@ -164,40 +173,19 @@ def test_fb_estimate_transform(fb_stitcher, left_img, right_img, not_to_bee):
 @pytest.mark.slow
 def test_overall_fb_stitching(fb_stitcher, left_img, right_img, outdir):
     fb_stitcher.estimate_transform(left_img['img'], right_img['img'], 90, -90)
+
     assert fb_stitcher.homo_left is not None and fb_stitcher.homo_right is not None
-    pano = fb_stitcher.compose_panorama(
-        left_img['img_w_detections'], right_img['img_w_detections'])
+
+    pano = fb_stitcher.compose_panorama(left_img['img_w_detections'], right_img['img_w_detections'])
+
     detections_left_mapped = fb_stitcher.map_left_points(left_img['detections'])
     detections_right_mapped = fb_stitcher.map_right_points(right_img['detections'])
+
     vis.draw_marks(pano, detections_left_mapped)
     vis.draw_marks(pano, detections_right_mapped)
 
     out = os.path.join(outdir, 'panorama_fb_w_detections.jpg')
     cv2.imwrite(out, pano)
-
-
-def test_rect_stitcher_estimate_transform(left_img, right_img, outdir, config,
-                                          monkeypatch):
-    def mockreturn(myself, image_list, all):
-        left_points = np.array([
-            [88.91666412, 3632.6015625],
-            [2760.26855469, 3636.70849609],
-            [2726.26708984, 363.4861145],
-            [93.88884735, 371.98330688]], dtype=np.float32)
-        right_points = np.array([
-            [181.49372864, 3687.71582031],
-            [2903.44042969, 3723.99926758],
-            [2921.27368164, 458.77352905],
-            [255.66642761, 431.24780273]], dtype=np.float32)
-        return left_points, right_points
-    monkeypatch.setattr(bb_stitcher.picking.picker.PointPicker, 'pick', mockreturn)
-
-    rt_stitcher = stitcher.RectangleStitcher(config)
-    rt_stitcher.estimate_transform(
-        left_img['img'], right_img['img'], 90, -90)
-    assert rt_stitcher.homo_left is not None
-    assert rt_stitcher.homo_right is not None
-    assert rt_stitcher.pano_size is not None
 
 
 @pytest.mark.slow
@@ -218,13 +206,16 @@ def test_overall_rt_stitching(left_img, right_img, outdir, config, monkeypatch):
 
     rt_stitcher = stitcher.RectangleStitcher(config)
     rt_stitcher.estimate_transform(left_img['img'], right_img['img'], 90, -90)
+
     assert rt_stitcher.homo_left is not None
     assert rt_stitcher.homo_right is not None
     assert rt_stitcher.pano_size is not None
-    pano = rt_stitcher.compose_panorama(
-        left_img['img_w_detections'], right_img['img_w_detections'])
+
+    pano = rt_stitcher.compose_panorama(left_img['img_w_detections'], right_img['img_w_detections'])
+
     detections_left_mapped = rt_stitcher.map_left_points(left_img['detections'])
     detections_right_mapped = rt_stitcher.map_right_points(right_img['detections'])
+
     vis.draw_marks(pano, detections_left_mapped)
     vis.draw_marks(pano, detections_right_mapped)
 
