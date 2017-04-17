0189a0c56b2d07b7ae26b1c77450db386921332a
gitmirgut <gitmirgut@users.noreply.github.com>
2017-04-17 16:05:06 +0200

    adjust sticher

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
-"""
-
-"""
+"""This is the setup file to install the bb_stitcher."""
+from pip.req import parse_requirements
 from setuptools import setup
+
+
+install_reqs = parse_requirements('requirements.txt', session=False)
+reqs = [str(ir.req) for ir in install_reqs]
+dep_links = [str(req_line.url) for req_line in install_reqs]
 
 setup(
     name='bb_stitcher',
@@ -15,9 +19,11 @@ setup(
             ]
     },
     url='https://github.com/gitmirgut/bb_stitcher',
+    install_requires=reqs,
+    dependency_links=dep_links,
     author='gitmirgut',
     author_email="gitmirgut@users.noreply.github.com",
-    packages=['bb_stitcher', 'bb_stitcher.picking'],
+    packages=['bb_stitcher', 'bb_stitcher.picking', 'bb_stitcher.scripts'],
     license='Apache License 2.0',
     classifiers=[
         'Development Status :: 3 - Alpha',
@@ -25,6 +31,6 @@ setup(
         'Programming Language :: Python :: 3.5'
     ],
     package_data={
-        'bb_stitcher': ['default_config.ini']
+        'bb_stitcher': ['*.ini']
     }
 )
